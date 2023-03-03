74032d41a671885026a45289497048462d2e8381
Victor Lee Korn <vileekorn@gmail.com>
2023-03-03 14:55:44 +0300

    change fastlane

--- .github/scripts/decrypt_android_keys.sh
+++ .github/scripts/decrypt_android_keys.sh
@@ -3,3 +3,5 @@
 # RELEASE KEYSTORE
 echo "$ANDROID_UPLOAD_STORE_FILE" > release-key.keystore.asc
 gpg -d --passphrase="$ANDROID_UPLOAD_STORE_FILE_PASSPHRASE" --batch release-key.keystore.asc > fastlane/release-key.keystore
+
+rm release-key.keystore.asc
