da265b7dcbf63a130a8dcc1a8d1a1b72734cfc6a
Victor Lee Korn <vileekorn@gmail.com>
2023-03-03 20:23:04 +0300

    added changes

--- .github/scripts/decrypt_android_keys.sh
+++ .github/scripts/decrypt_android_keys.sh
@@ -2,6 +2,6 @@
 
 # RELEASE KEYSTORE
 echo "$ANDROID_UPLOAD_STORE_FILE" > release-key.keystore.asc
-gpg -d --passphrase="$ANDROID_UPLOAD_STORE_FILE_PASSPHRASE" --batch release-key.keystore.asc > fastlane/release-key.keystore
+gpg -d --passphrase="$ANDROID_UPLOAD_STORE_FILE_PASSPHRASE" --batch release-key.keystore.asc > android/fastlane/release-key.keystore
 
 rm release-key.keystore.asc
